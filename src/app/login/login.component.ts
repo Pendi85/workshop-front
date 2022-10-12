0250766dd65280b7e360d242729250c09f9537ad
Pendi85 <81863481+Pendi85@users.noreply.github.com>
2022-10-12 10:50:46 +0200

    Configuration of login form completed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm : FormGroup;
+  submitted : boolean;
 
-  constructor() { }
+  constructor(private router : Router) {
+    this.loginForm = new FormGroup({
+      emailControl : new FormControl('', Validators.email),
+      passwordControl : new FormControl('', Validators.required)
+    })
+
+    this.submitted = false;
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+    this.submitted = true;
+    const regexValidationEmail = /^\S+@\S+\.\S+$/;
+        
+    //Create Session
+    if(this.emailControl.value.length > 0 && this.passwordControl.value.length > 0
+      && regexValidationEmail.test(this.emailControl.value)) {
+      console.log(this.loginForm.getRawValue());
+      this.router.navigate(['']);
+    }
+  }
+
+  get emailControl(): any {
+    return this.loginForm.get('emailControl');
+  }
+
+  get passwordControl(): any {
+    return this.loginForm.get('passwordControl');
+  }
 }
